6e1cf54687b3e19820f76dc3812cf417a9ae9d25
Julio Rarick <juliorarick@gmail.com>
2024-08-01 14:16:11 -0300

    page necropolitc

--- app/necropolitic/page.tsx
+++ app/necropolitic/page.tsx
@@ -31,7 +31,7 @@ export default function Necropolitic() {
                   Achille Mbembe
                 </p>
                 <DialogCard
-                  buttonTitle="Ver biogrfia"
+                  buttonTitle="Ver biografia"
                   variant="default"
                   description="Achille Mbembe é um historiador e teórico político camaronês, nascido em 1957. Ele é conhecido por suas contribuições aos estudos pós-coloniais e à teoria crítica. Seus trabalhos exploram temas como colonialismo, globalização, biopolítica e necropolítica. Entre suas obras mais influentes estão (Necropolítica e Crítica da Razão Negra). Atualmente, Mbembe é professor na Universidade de Witwatersrand, na África do Sul."
                   title="Achille Mbembe"
@@ -49,7 +49,7 @@ export default function Necropolitic() {
                 </p>
                 <DialogCard
                   variant="default"
-                  buttonTitle="Ver biogrfia"
+                  buttonTitle="Ver biografia"
                   description="Michel Foucault foi um filósofo e historiador francês, nascido em 1926 e falecido em 1984. Ele é conhecido por suas análises sobre o poder, a sociedade e a subjetividade. Seus trabalhos abordam temas como a disciplina, a biopolítica e a história da sexualidade. Entre suas obras mais influentes estão Vigiar e Punir e A História da Sexualidade. Foucault é considerado um dos principais teóricos do século XX, tendo influenciado diversos campos das ciências humanas e sociais."
                   title="Michael Foucault"
                 />
