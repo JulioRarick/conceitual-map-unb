6b571ab606801d63501427f8c7a23f3e707d70a4
Julio Rarick <juliorarick@gmail.com>
2024-09-03 09:23:02 -0300

    modify Foucault biography on data-authors-necropolitic

--- data/data-authors-necropolitic.ts
+++ data/data-authors-necropolitic.ts
@@ -24,6 +24,6 @@ export const achileMbembeEnglishData: AuthorsNecropoliticDataTypes = {
 export const foucaultEnglishData: AuthorsNecropoliticDataTypes = {
   name: 'Michael Foucault',
   description:
-    'Michel Foucault foi um filósofo e historiador francês, nascido em 1926 e falecido em 1984. Ele é conhecido por suas análises sobre o poder, a sociedade e a subjetividade. Seus trabalhos abordam temas como a disciplina, a biopolítica e a história da sexualidade. Entre suas obras mais influentes estão Vigiar e Punir e A História da Sexualidade. Foucault é considerado um dos principais teóricos do século XX, tendo influenciado diversos campos das ciências humanas e sociais.',
-  buttonTitle: 'Ver biografia',
+    'Michel Foucault was a French philosopher and historian, born in 1926 and passed away in 1984. He is known for his analyses of power, society, and subjectivity. His work covers topics such as discipline, biopolitics, and the history of sexuality. Among his most influential works are Discipline and Punish and The History of Sexuality. Foucault is considered one of the leading theorists of the 20th century, having influenced various fields within the humanities and social sciences.',
+  buttonTitle: 'Biography',
 }
